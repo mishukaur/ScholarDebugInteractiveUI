34a435f79288e2ecbb7db3a06415e07dcacb5346
Manjot Kaur <manjotk2003@gmail.com>
2025-04-23 17:40:39 -0400

    Initial Commit

---
+++ ScholarDebugInteractiveUI/ScholarDebugInteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarDebugInteractiveUIApp.swift
+//  ScholarDebugInteractiveUI
+//
+//  Created by Manjot Kaur on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ScholarDebugInteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
